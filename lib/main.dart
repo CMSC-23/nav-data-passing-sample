8a5bea2f8902c4e88d92bee83d5c234462788758
claizelcoubeili <clcepe@up.edu.ph>
2022-02-11 00:31:59 +0800

    Added argument screen with selection

--- lib/main.dart
+++ lib/main.dart
@@ -12,39 +12,6 @@ class MyApp extends StatelessWidget {
         ExtractArgumentsScreen.routeName: (context) =>
             const ExtractArgumentsScreen(),
       },
-      // Provide a function to handle named routes.
-      // Use this function to identify the named
-      // route being pushed, and create the correct
-      // Screen.
-      onGenerateRoute: (settings) {
-        // If you push the PassArguments route
-        if (settings.name == PassArgumentsScreen.routeName) {
-          // Cast the arguments to the correct
-          // type: ScreenArguments.
-          final args = settings.arguments as ScreenArguments;
-
-          // Then, extract the required data from
-          // the arguments and pass the data to the
-          // correct screen.
-          return MaterialPageRoute(
-            builder: (context) {
-              return PassArgumentsScreen(
-                title: args.title,
-                message: args.message,
-              );
-            },
-          );
-        }
-        // The code only supports
-        // PassArgumentsScreen.routeName right now.
-        // Other values need to be implemented if we
-        // add them. The assertion here will help remind
-        // us of that higher up in the call stack, since
-        // this assertion would otherwise fire somewhere
-        // in the framework.
-        assert(false, 'Need to implement ${settings.name}');
-        return null;
-      },
       title: 'Navigation with Arguments',
       home: const HomeScreen(),
     );
@@ -84,27 +51,6 @@ class HomeScreen extends StatelessWidget {
               },
               child: const Text('Navigate to screen that extracts arguments'),
             ),
-            // A button that navigates to a named route.
-            // For this route, extract the arguments in
-            // the onGenerateRoute function and pass them
-            // to the screen.
-            ElevatedButton(
-              onPressed: () {
-                // When the user taps the button, navigate
-                // to a named route and provide the arguments
-                // as an optional parameter.
-                Navigator.pushNamed(
-                  context,
-                  PassArgumentsScreen.routeName,
-                  arguments: ScreenArguments(
-                    'Accept Arguments Screen',
-                    'This message is extracted in the onGenerateRoute '
-                        'function.',
-                  ),
-                );
-              },
-              child: const Text('Navigate to a named that accepts arguments'),
-            ),
           ],
         ),
       ),
@@ -130,41 +76,30 @@ class ExtractArgumentsScreen extends StatelessWidget {
         title: Text(args.title),
       ),
       body: Center(
-        child: Text(args.message),
-      ),
-    );
-  }
-}
-
-// A Widget that accepts the necessary arguments via the
-// constructor.
-class PassArgumentsScreen extends StatelessWidget {
-  static const routeName = '/passArguments';
-
-  final String title;
-  final String message;
-
-  // This Widget accepts the arguments as constructor
-  // parameters. It does not extract the arguments from
-  // the ModalRoute.
-  //
-  // The arguments are extracted by the onGenerateRoute
-  // function provided to the MaterialApp widget.
-  const PassArgumentsScreen({
-    Key? key,
-    required this.title,
-    required this.message,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(message),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(args.message),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, 'Yep!');
+              },
+              child: const Text('Yep!'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, 'Nope.');
+              },
+              child: const Text('Nope.'),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
